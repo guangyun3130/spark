b8196c2ed113fad58002c5c559b4b3a271188194
Enrico Minack <github@enrico.minack.dev>
2024-09-06 14:14:58 +0200

    Reuse ScopedExpression child name

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/analysis/Analyzer.scala
@@ -458,7 +458,11 @@ class Analyzer(override val catalogManager: CatalogManager) extends RuleExecutor
         case e if !e.resolved => u
         case g: Generator => MultiAlias(g, Nil)
         case c @ Cast(ne: NamedExpression, _, _, _) => Alias(c, ne.name)()
-        case se @ ScopedExpression(ne: NamedExpression, _) => Alias(se, ne.name)()
+        case se @ ScopedExpression(e: Expression, _) =>
+          resolve(UnresolvedAlias(e, optGenAliasFunc)) match {
+            case ne: NamedExpression => Alias(se, ne.name)()
+            case _ => se
+          }
         case e: ExtractValue if extractOnly(e) => Alias(e, toPrettySQL(e))()
         case e if optGenAliasFunc.isDefined =>
           Alias(child, optGenAliasFunc.get.apply(e))()
